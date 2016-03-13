ef834a2e72ea189b64af8634e771bfcd35998dc6
Przemek Burczyk <przemek.burczyk@gmail.com>
2016-03-13 17:59:18 +0100

    Implemented image displaying.

--- app/src/main/java/pl/edu/agh/marims/demo/MainActivity.java
+++ app/src/main/java/pl/edu/agh/marims/demo/MainActivity.java
@@ -1,6 +1,8 @@
 package pl.edu.agh.marims.demo;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -11,12 +13,36 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.LinkedHashMap;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button toggleImageButton;
+    private ImageView imageView;
     private EditText emailAddressField;
     private Handler handler = new Handler();
+
+    private static final int MAX_BITMAP_CACHE_SIZE = 3;
+    private LinkedHashMap<Integer, Bitmap> bitmapCache = new LinkedHashMap<Integer, Bitmap>(MAX_BITMAP_CACHE_SIZE) {
+        @Override
+        protected boolean removeEldestEntry(Entry<Integer, Bitmap> eldest) {
+            return false;
+        }
+    };
+    private int[] bitmaps = new int[]{
+            R.drawable.bitmap1,
+            R.drawable.bitmap2,
+            R.drawable.bitmap3,
+            R.drawable.bitmap4,
+            R.drawable.bitmap5,
+            R.drawable.bitmap6,
+            R.drawable.bitmap7
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +50,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toggleImageButton = (Button) findViewById(R.id.toggleImageButton);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        toggleImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (imageView.getVisibility() == View.GONE) {
+                    int bitmapIndexToDisplay = new Random().nextInt(bitmaps.length);
+                    Bitmap bitmap;
+                    if (bitmapCache.containsKey(bitmapIndexToDisplay)) {
+                        bitmap = bitmapCache.get(bitmapIndexToDisplay);
+                    } else {
+                        bitmap = BitmapFactory.decodeResource(getResources(), bitmaps[bitmapIndexToDisplay]);
+                        bitmapCache.put(bitmapIndexToDisplay, bitmap);
+                    }
+                    imageView.setImageBitmap(bitmap);
+                    imageView.setVisibility(View.VISIBLE);
+                } else {
+                    imageView.setVisibility(View.GONE);
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
